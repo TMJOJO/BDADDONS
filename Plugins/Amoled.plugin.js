1c78e0458fc93bbc37b9dd778c06ef05734d8a5a
TMJOJO <126905455+TMJOJO@users.noreply.github.com>
2023-03-03 20:49:10 +0300

    Update Amoled.plugin.js

--- Plugins/Amoled.plugin.js
+++ Plugins/Amoled.plugin.js
@@ -3,12 +3,14 @@
  * @author Jotaro
  * @version 0.0.1
  * @description Makes discord visually easier on the eyes
+ * @source https://github.com/TMJOJO/BDADDONS/main/Plugins/Amoled.plugin.js
+ * @updateUrl https://raw.githubusercontent.com/TMJOJO/BDADDONS/main/Plugins/Amoled.plugin.js
  */
 
 class AmoledMode {
   constructor() {
   }
-
+/
   start() {
     document.body.classList.add("theme-amoled");
   }
