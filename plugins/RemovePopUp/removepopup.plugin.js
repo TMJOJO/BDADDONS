e2ee35d946e39a378a75215033530a9df2c1062b
TMJOJO <126905455+TMJOJO@users.noreply.github.com>
2023-04-07 20:20:02 +0300

    ty stern, bettlee and yentis

--- plugins/RemovePopUp/removepopup.plugin.js
+++ plugins/RemovePopUp/removepopup.plugin.js
@@ -4,13 +4,16 @@
  * @version 0.1.0
  * @description remove every popup
  * @source https://github.com/TMJOJO/BDADDONS
- * @updateURL https://raw.githubusercontent/TMJOJO/BDADDONS/Removepopup.plugin.js
+ * @updateURL https://raw.githubusercontent/TMJOJO/BDADDONS/Removepopups.plugin.js
  */
 
 class rpopup {
+  observe;
 
 start() { 
-const observer = new MutationObserver(function(mutations_list) {
+	// ty to stern and bettlee for teaching me about mutation observers
+ // and ty to yentis for some code
+this.observer = new MutationObserver(function(mutations_list) {
 	mutations_list.forEach(function(mutation) {
 		mutation.addedNodes.forEach(function(added_node) {
 				 var element = document.getElementsByClassName("backdrop-2ByYRN withLayer-2VVmpp");
@@ -22,22 +25,12 @@ const observer = new MutationObserver(function(mutations_list) {
 	});
 });
 
-observer.observe(document.querySelector("#app-mount"), { subtree: true, childList: true });
+this.observer.observe(document.querySelector("#app-mount"), { subtree: true, childList: true });
 }
   stop() {
-    // since discord will recreate the elements we dont need anythin here
- BdApi.showNotice(
-    "You need to restart discord inorder to disable Removepopups",
-    {
-        type: "error",
-        buttons: [
-            {
-                label: "Restart",
-                onClick: () => location.reload(true)
-            },
-        ]
-    }
-);
+   // ty to yentis for this right here
+ this.observer.disconnect();
+    this.observer = undefined;
   }
 
 
